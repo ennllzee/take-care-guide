9fe2d3f04e4098aa2f5544e29a97be219284d31a
ennllzee <nattha_c@cmu.ac.th>
2021-10-08 03:30:33 +0700

    profile

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -1,76 +1,93 @@
-import { CardContent, CardMedia, createStyles, Grid, makeStyles, Theme, Typography, Card } from "@material-ui/core"
-import { history } from "../../helper/history"
-import { GoogleLogout } from 'react-google-login';
+import {
+  CardContent,
+  CardMedia,
+  createStyles,
+  Grid,
+  makeStyles,
+  Theme,
+  Typography,
+  Card,
+  Button,
+} from "@material-ui/core";
+import { history } from "../../helper/history";
+import { useGoogleLogout } from "react-google-login";
+import { ExitToApp } from "@material-ui/icons";
 
-interface ProfileCardProps{
-    name: string
-    gmail?: string
-    img: any
+interface ProfileCardProps {
+  name: string;
+  gmail?: string;
+  img: any;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        card: {
-            display: 'flex',
-        },
-        details: {
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '2%',
-            width: "60%"
-        },
-        content: {
-            flex: '1 0 auto',
-        },
-        cover: {
-            width: "40%",
-        },
-        logout: {
-            display: 'flex',
-            paddingLeft: theme.spacing(1),
-            paddingBottom: theme.spacing(1),
-        }
-    })
-)
+  createStyles({
+    card: {
+      display: "flex",
+    },
+    details: {
+      display: "flex",
+      flexDirection: "column",
+      padding: "2%",
+      width: "60%",
+    },
+    content: {
+      flex: "1 0 auto",
+    },
+    cover: {
+      width: "40%",
+    },
+    logout: {
+      display: "flex",
+      paddingLeft: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+    },
+  })
+);
 
-function ProfileCard({name, gmail, img} : ProfileCardProps){
+function ProfileCard({ name, gmail, img }: ProfileCardProps) {
+  const classes = useStyles();
 
-    const classes = useStyles()
+  const logout = () => {
+    localStorage.clear();
+    history.push("/");
+  };
 
-    const logout = () => {
-        localStorage.clear()
-        history.push("/")
-    }
+  const { signOut } = useGoogleLogout({
+    clientId:
+      "907374215732-jc7l3sk84f05vlsf9e23ceo674ek0sbe.apps.googleusercontent.com",
+    onLogoutSuccess: logout,
+  });
 
-    return (
-        <Card className={classes.card}>
-            <CardMedia
-            className={classes.cover}
-            image={img}
-            title={name}
-            />
-            <div className={classes.details}>
-                <CardContent className={classes.content}>
-                    <Typography component="h6" variant="h6">
-                        {name}
-                        <br/>
-                        <Typography variant="caption" color="textSecondary">
-                            {gmail}
-                        </Typography>
-                    </Typography>
-                </CardContent>
-                <Grid container justify="flex-end" className={classes.logout}>
-                    <GoogleLogout
-                    clientId="907374215732-jc7l3sk84f05vlsf9e23ceo674ek0sbe.apps.googleusercontent.com"
-                    buttonText="Logout"
-                    onLogoutSuccess={logout}
-                    icon={false}
-                    >
-                    </GoogleLogout>
-                </Grid>
-            </div>
-        </Card>
-    )
+  return (
+    <Card className={classes.card}>
+      <CardMedia className={classes.cover} image={img} title={name} />
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h6" variant="h6">
+            {name}
+            <br />
+            <Typography variant="caption" color="textSecondary">
+              {gmail}
+            </Typography>
+          </Typography>
+        </CardContent>
+        <Grid container justify="flex-end" className={classes.logout}>
+          <Button onClick={signOut} style={{ padding: "3%" }}>
+            <Grid
+              container
+              direction="row"
+              spacing={1}
+              justify="center"
+              alignItems="center"
+            >
+              <ExitToApp />
+              <Typography variant="body1">ลงชื่อออก</Typography>
+            </Grid>
+          </Button>
+        </Grid>
+      </div>
+    </Card>
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
