38c6da62115c81691156dbdd3eada6f271c2c8f0
ennllzee <nattha_c@cmu.ac.th>
2021-10-10 00:15:20 +0700

    new

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -54,36 +54,6 @@ interface TopBarProps {
 
 function TopBar({ page }: TopBarProps) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null
-  );
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-    setOpen((prev) => !prev);
-  };
-
-  const accessToken = localStorage.getItem("accessToken");
-
-  const { GET_ALL_APPOINTMENT_BY_GUIDE } = useGuideApi();
-  const id = localStorage.getItem("_id");
-
-  const { loading, error, data } = useQuery(GET_ALL_APPOINTMENT_BY_GUIDE, {
-    variables: { getAllAppointmentByGuideGuideId: id },
-    pollInterval: 1000,
-  });
-
-  const [appointment, setAppointment] = useState<Appointment[]>(
-    data !== undefined ? data.getAllAppointmentByGuide : []
-  );
-
-  useEffect(() => {
-    if (!loading && data) {
-      setAppointment(data.getAllAppointmentByGuide);
-    }
-    if (error) console.log(error?.graphQLErrors);
-  }, [loading, data, error]);
 
   const logout = () => {
     localStorage.clear();
@@ -97,81 +67,7 @@ function TopBar({ page }: TopBarProps) {
           <Typography variant="h4" className={classes.title}>
             {page}
           </Typography>
-          {accessToken !== null ? (
-            <>
-              <Popper
-                open={open}
-                anchorEl={anchorEl}
-                placement="bottom-end"
-                transition
-              >
-                {({ TransitionProps }) => (
-                  <Fade {...TransitionProps} timeout={350}>
-                    <Paper className={classes.typography}>
-                      {!loading ? (
-                        <>
-                          {appointment !== undefined &&
-                          appointment.find((a) => a.Status.Tag === "Waiting for Guide to Confirm") ? (
-                            appointment
-                              ?.filter((a) => a.Status.Tag === "Waiting for Guide to Confirm")
-                              .slice()
-                              .sort((a, b) => {
-                                return (
-                                  new Date(a.AppointTime).getTime() -
-                                  new Date(b.AppointTime).getTime()
-                                );
-                              })
-                              .map((a) => {
-                                return (
-                                  <>
-                                    <Typography>
-                                      {convertToThaiDate(new Date(a.AppointTime))}
-                                    </Typography>
-                                    <Divider />
-                                    <Typography>
-                                      โรงพยาบาล: {a.Hospital.Name}
-                                    </Typography>
-                                    <Typography>
-                                      แผนก: {a.Department.Name}
-                                    </Typography>
-                                    <Typography>
-                                      เวลานัดหมาย:{" "}
-                                      {moment(new Date(a.AppointTime)).format(
-                                        "HH.mm น."
-                                      )}
-                                    </Typography>
-                                  </>
-                                );
-                              })
-                          ) : (
-                            <Typography
-                              align="center"
-                              variant="subtitle1"
-                              color="textSecondary"
-                            >
-                              ไม่มีการนัดหมาย
-                            </Typography>
-                          )}
-                        </>
-                      ) : (
-                        <Grid
-                          container
-                          direction="row"
-                          alignItems="center"
-                          justify="center"
-                        >
-                          <CircularProgress disableShrink />
-                        </Grid>
-                      )}
-                    </Paper>
-                  </Fade>
-                )}
-              </Popper>
-              <IconButton className={classes.icon} onClick={handleClick()}>
-                <ViewList />
-              </IconButton>
-            </>
-          ) : page === "ลงทะเบียน" ? (
+          {page === "ลงทะเบียน" && (
             <GoogleLogout
               clientId="907374215732-jc7l3sk84f05vlsf9e23ceo674ek0sbe.apps.googleusercontent.com"
               buttonText="Login"
@@ -187,8 +83,6 @@ function TopBar({ page }: TopBarProps) {
               onLogoutSuccess={logout}
               icon={false}
             ></GoogleLogout>
-          ) : (
-            <></>
           )}
         </Toolbar>
       </AppBar>
