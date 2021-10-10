8ba22c007d5d883938dfebf68d1cf7b56aafd13b
ennllzee <nattha_c@cmu.ac.th>
2021-10-10 15:14:30 +0700

    price

--- src/components/History/AppointmentCard.tsx
+++ src/components/History/AppointmentCard.tsx
@@ -196,6 +196,16 @@ function AppointmentCard({ appointment }: AppointmentCardProps) {
             </Grid>
             <Grid item xs={5}>
               <Typography variant="body1" align="left">
+                ค่าบริการ:
+              </Typography>
+            </Grid>
+            <Grid item xs={7}>
+              <Typography variant="body1" align="left">
+                {appointment.Price}
+              </Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <Typography variant="body1" align="left">
                 ระดับความพึงพอใจ:
               </Typography>
             </Grid>
